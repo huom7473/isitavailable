4a572ec5a77a971082e0f500b478f0d6e94384d1
huom7473 <huom7473@g.ucla.edu>
2020-05-19 22:24:21 -0700

    Added basic testing locations, interactivity features

--- isitavailable/src/App.js
+++ isitavailable/src/App.js
@@ -3,28 +3,49 @@ import {
   withGoogleMap,
   withScriptjs,
   GoogleMap,
-  //Marker,
-  //InfoWindow
+  Marker,
+  InfoWindow 
 } from "react-google-maps";
+import POIs from "./data/POIs.json"
 
 //import mapStyles from "./mapStyles";
 
-function Map() {
+function RawMap() {
+  const [selectedPOI, setSelected] = useState(null);
+
   return (
     <GoogleMap
-      defaultZoom={10}
-      defaultCenter={{lat : 34.069, lng: -118.445}}
-    />
+      defaultZoom={15.5}
+      defaultCenter={{lat : 34.069, lng: -118.445}} //start at UCLA for now
+    >
+        {POIs.map(place => 
+        (<Marker
+          key = {place.NAME}
+          position = {{lat: place.COORDINATES[0], lng: place.COORDINATES[1]}}
+          onClick = {() => setSelected(place)}/>)
+          )}
+          {selectedPOI && //boolean "trick" used here - might want to change to something a bit less cryptic in the future
+          <InfoWindow
+            position = {{lat: selectedPOI.COORDINATES[0], lng: selectedPOI.COORDINATES[1]}}
+            onCloseClick = {() => setSelected(null)}
+            >
+              <div>
+                <h2>{selectedPOI.NAME}</h2>
+                <p>{selectedPOI.DESCRIPTION}</p>
+              </div>
+          </InfoWindow>
+          }
+    </GoogleMap>
   );
 }
 
 
-const MapWrapped = withScriptjs(withGoogleMap(Map));
+const Map = withScriptjs(withGoogleMap(RawMap));
 
 export default function App() {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-      <MapWrapped
+      <Map
         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${
           process.env.REACT_APP_GOOGLE_KEY
         }`}
