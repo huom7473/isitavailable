ec427063821b5d87ff5faa302777eb91e515722c
huom7473 <huom7473@g.ucla.edu>
2020-05-21 16:10:38 -0700

    changed Map from a function component to React class component

--- isitavailable/src/App.js
+++ isitavailable/src/App.js
@@ -19,38 +19,44 @@ const defaultCenter = {lat : 34.069, lng: -118.445};
 const defaultZoom = 15.5;
 const options = {styles: mapTheme}
 
-function Map() {
-  const [selectedPOI, setSelected] = React.useState(null);
+class Map extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {selectedPOI: null};
+  }
 
-  return (
-    <GoogleMap 
-          mapContainerStyle={mapContainerStyles}
-          zoom={defaultZoom}
-          center={defaultCenter}
-          options = {options}>
-        {POIs.map(place => 
-        (<Marker
-          key = {place.NAME}
-          position = {{lat: place.COORDINATES[0], lng: place.COORDINATES[1]}}
-          onClick = {() => setSelected(place)}
-          icon = {{
-            url: icons[place.TYPE],
-            scaledSize: new window.google.maps.Size(40, 40)}}
-          />)
-          )}
-          {selectedPOI && //boolean "trick" used here - might want to change to something a bit less cryptic in the future
-          <InfoWindow
-            position = {{lat: selectedPOI.COORDINATES[0], lng: selectedPOI.COORDINATES[1]}}
-            onCloseClick = {() => setSelected(null)}
-            >
-              <div>
-                <h2>{selectedPOI.NAME}</h2>
-                <p>{selectedPOI.DESCRIPTION}</p>
-              </div>
-          </InfoWindow>
-          }
-    </GoogleMap>
-  );
+  render() {
+
+    return (
+      <GoogleMap 
+            mapContainerStyle={mapContainerStyles}
+            zoom={defaultZoom}
+            center={defaultCenter}
+            options = {options}>
+          {POIs.map(place => 
+          (<Marker
+            key = {place.NAME}
+            position = {{lat: place.COORDINATES[0], lng: place.COORDINATES[1]}}
+            onClick = {() => this.setState({selectedPOI: place})}
+            icon = {{
+              url: icons[place.TYPE],
+              scaledSize: new window.google.maps.Size(40, 40)}}
+            />)
+            )}
+            {this.state.selectedPOI && //boolean "trick" used here - might want to change to something a bit less cryptic in the future
+            <InfoWindow
+              position = {{lat: this.state.selectedPOI.COORDINATES[0], lng: this.state.selectedPOI.COORDINATES[1]}}
+              onCloseClick = {() => this.setState({selectedPOI: null})}
+              >
+                <div>
+                  <h2>{this.state.selectedPOI.NAME}</h2>
+                  <p>{this.state.selectedPOI.DESCRIPTION}</p>
+                </div>
+            </InfoWindow>
+            }
+      </GoogleMap>
+    );
+    }
 }
 
 
