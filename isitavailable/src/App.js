1ee6c43df6b67513929f9a8c9a7e38bd162a9679
huom7473 <huom7473@g.ucla.edu>
2020-06-04 20:42:56 -0700

    clear POIs hotfix

--- isitavailable/src/App.js
+++ isitavailable/src/App.js
@@ -58,7 +58,7 @@ export default function App() {
 
     const removeMarkers = React.useCallback(() => {
       //console.log(markers);
-      for(let i = 0; i < markers.length; i++){
+      for(let i = 0; i < global.markers.length; i++){
         global.markers[i].setMap(null);
       }
       global.markers = [];
